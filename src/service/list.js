e99038f9249a44bea53aae13a3461d71b6c6abc7
Shallowbeing <load_more@163.com>
2021-06-19 15:30:01 +0800

    feat: 完善删除列表功能，实现级联删除

--- src/service/list.js
+++ src/service/list.js
@@ -1,4 +1,5 @@
 const List = require('../db/model/list')
+const Item = require('../db/model/item')
 
 async function createListService({ title, description, userid }) {
   const rst = await List.create({
@@ -22,6 +23,12 @@ async function editListService({listid, title, description}) {
 }
 
 async function deleteListService(listid) {
+  // 级联删除，先删除关联的子项，后删除列表
+  await Item.destroy({
+    where: {
+      listid
+    }
+  })
   const rst = await List.destroy({
     where: {
       id: listid
